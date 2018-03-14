f60924eb725f8b43af2373e5ac3bc5cc494af92c
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-03-14 11:36:35 -0300

    initial commit

---
+++ contacts/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts';
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+  
+  render() {
+    return (
+      <div>
+        <ListContacts contacts={this.state.contacts} />
+      </div>
+    )
+  }
+}
+
+export default App;
